0c1190e4a969c98bfe4c9b5912f1c44377f645c7
xyyangkun <xyyangkun@163.com>
2013-09-12 10:15:52 +0800

    添加test

---
+++ Makefile
@@ -0,0 +1,3 @@
+CC=gcc
+all:test
+	gcc src/test.c -o test
---
+++ src/test.c
@@ -0,0 +1,13 @@
+/*
+ * test.c
+ *
+ *  Created on: 2013-9-12
+ *      Author: yangkun
+ */
+#include <stdio.h>
+
+int main()
+{
+	printf("hello\n");
+	return 0;
+}
